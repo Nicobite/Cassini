bd332c9c87af5963b92eca2b972cfb97a7b40196
thiebaud <thiebaud@15f1c17f-2811-4e6c-9d15-9a1561b364c4>
2014-02-19 17:06:37 +0000

    Images

--- Cassini/src/org/insa/view/TitleMenu.java
+++ Cassini/src/org/insa/view/TitleMenu.java
@@ -39,12 +39,12 @@ public class TitleMenu extends BorderPane{
     public TitleMenu() {
         this.setStyle("-fx-background-color: #272A2D;");
         
-        helpLabel.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/help.png"))));
+        helpLabel.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/org/insa/view/help.png"))));
         helpLabel.setPrefWidth(50);
         helpLabel.setPrefHeight(50);
         helpLabel.setAlignment(Pos.CENTER);
         
-        contactLabel.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/contact.png"))));
+        contactLabel.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/org/insa/view/contact.png"))));
         contactLabel.setPrefWidth(50);
         contactLabel.setPrefHeight(50);
         contactLabel.setAlignment(Pos.CENTER);
