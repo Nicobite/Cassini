300b0b4c0290a1895449fc0d2a52e56702205a74
aissaoui <aissaoui@15f1c17f-2811-4e6c-9d15-9a1561b364c4>
2014-05-09 15:05:50 +0000

    TrafficLight implementation

--- Cassini/src/org/insa/core/trafficcontrol/TrafficLight.java
+++ Cassini/src/org/insa/core/trafficcontrol/TrafficLight.java
@@ -15,11 +15,85 @@
  */
 package org.insa.core.trafficcontrol;
 
+import org.insa.core.enums.StateTrafficLight;
+import org.insa.core.roadnetwork.Node;
+
 /**
  *
  * @author Juste Abel Ouedraogo & Guillaume Garzone & François Aïssaoui &  Thomas Thiebaud
  Class TrafficLight
  */
 public class TrafficLight {
+    
+    private Node position ; 
 
+    private int counter ;
+    
+    private int redTime ;
+    private int orangeTime ;
+    private int greenTime ;
+    
+    private StateTrafficLight state ;
+
+    public TrafficLight(Node position, int redTime, int orangeTime, int greenTime) {
+        this.position = position;
+        this.redTime = redTime;
+        this.orangeTime = orangeTime;
+        this.greenTime = greenTime;
+        this.state = StateTrafficLight.RED ; 
+    }
+    
+    
+    public Node getPosition() {
+        return position;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public int getRedTime() {
+        return redTime;
+    }
+
+    public int getOrangeTime() {
+        return orangeTime;
+    }
+
+    public int getGreenTime() {
+        return greenTime;
+    }
+
+    public void setPosition(Node position) {
+        this.position = position;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public void setRedTime(int redTime) {
+        this.redTime = redTime;
+    }
+
+    public void setOrangeTime(int orangeTime) {
+        this.orangeTime = orangeTime;
+    }
+
+    public void setGreenTime(int greenTime) {
+        this.greenTime = greenTime;
+    }
+
+    public StateTrafficLight getState() {
+        return state;
+    }
+
+    public void setState(StateTrafficLight state) {
+        this.state = state;
+    }
+
+
+    
+    
+    
 }
